fb1691d708353165b131c19206fa50610a6254eb
Gauss Jordan <gausjordan@outlook.com>
2023-07-04 13:59:33 +0200

    Randomizer 2

--- flow.js
+++ flow.js
@@ -6,6 +6,7 @@ let canvasCtx = canvasRef.getContext('2d');
 canvasRef.height = flexMain.clientHeight;
 canvasRef.width = flexMain.clientWidth;
 
+
 // Get pixel color values at mouse position, on click event
 let posX = 0;
 let posY = 0;
@@ -22,7 +23,8 @@ window.addEventListener("click", (ev) => {
 window.addEventListener("resize", (ev) => {
     canvasRef.height = flexMain.clientHeight;
     canvasRef.width = flexMain.clientWidth;
-    drawTerrain(terrain);
+    let pixelData = generateContour(cPoints);
+    drawPixelData(pixelData);
 });
 
 
@@ -42,48 +44,33 @@ function randomInteger(lower, upper) {
     return Math.floor(Math.random() * (upper - lower + 1)) + lower;
 }
 
-// Creates random landscape data
-function createTerrain() {
-    var bezierPoints = generatePoints();
-    return bezierPoints;
-}
 
-// Generates random curve control points (landscape)
-function generatePoints() {
-    let bezierPoints = [];
+// Create random (landscape) curve control points
+function generateControlPoints() {
+    let points = [];
     for (let i=0; i < 12; i++) {
-        bezierPoints[i] = randomInteger(50, 600);
-        
-        // var bezierPoints = [360,75,241,128,109,503,261,274,
-        //                     419,499,188,194,494,158,80,557,
-        //                     158,471,336,118,435,63,475,322,
-        //                     439,460,177,331,224,204,556,327];
+        points[i] = randomInteger(50, 600);
     }
-    return bezierPoints;
+    return points;
 }
 
+// Takes control points in, returns an array of heights (landscape contour)
+function generateContour(cPts) {
 
-function generateContour(terrain) {
-    let screenBuffer = new Array(canvasRef.width).fill(0);
-    let pxPerSeg = Math.ceil((canvasRef.width) / (terrain.length-4));
-    let stepPerPx = 1 / ((canvasRef.width) / (terrain.length-4));
-    console.log("pxPerSegment: " + pxPerSeg);
-    console.log("StepsPerPx: " + stepPerPx);
+    let contour = new Array(canvasRef.width).fill(0);
+    let pxPerSeg = Math.ceil((canvasRef.width) / (cPts.length-4));
+    let stepPerPx = 1 / ((canvasRef.width) / (cPts.length-4));
 
-    for (let seg=0; seg < terrain.length-4; seg++) {
+    for (let seg=0; seg < cPts.length-4; seg++) {
         for (let px=0, step=0; px<pxPerSeg, step<1; px++, step += stepPerPx) {
-            screenBuffer[(seg * pxPerSeg) + px] =
+            contour[(seg * pxPerSeg) + px] =
                 Math.round(
-                    catmullRom(
-                        terrain[seg],
-                        terrain[seg+1],
-                        terrain[seg+2],
-                        terrain[seg+3],
-                        step)
+                    catmullRom( cPts[seg], cPts[seg+1],
+                    cPts[seg+2], cPts[seg+3], step)
                 );
         }
     }
-    return screenBuffer;    
+    return contour;    
 }
 
 
@@ -95,23 +82,7 @@ function drawPixelData(pixelData) {
 }
 
 
-function drawTerrain(bezierPoints) {
-
-    canvasCtx.fillStyle = "rgb(255,0,255)";
-    canvasCtx.fillRect(canvasRef.width-1, canvasRef.height-1, 1, 1);
-    canvasCtx.stroke();
-
-    let currentPixel = 0;
-
-    for (let g = 0; g < bezierPoints.length; g++) {
-        for (let i = 0; i < 1; i += 0.01) {
-            canvasCtx.fillStyle = "rgba(0,0,0,0.2)";
-            currentPixel = catmullRom(bezierPoints[g+0], bezierPoints[g+1], bezierPoints[g+2], bezierPoints[g+3], i);
-            canvasCtx.fillRect(i*100+(g*100), currentPixel, 2, 2);
-        } 
-    }
-}
-
+// Returns a 4-point-curve segment value at position "t"
 function catmullRom(p0, p1, p2, p3, t) {
     return (
         2 * p1 +
@@ -122,11 +93,8 @@ function catmullRom(p0, p1, p2, p3, t) {
 }
 
 
-
-
-let terrain = createTerrain();
-let pixelData = generateContour(terrain);
-drawTerrain(terrain);
+let cPoints = generateControlPoints();
+let pixelData = generateContour(cPoints);
 drawPixelData(pixelData);
 
 
---
+++ urgaye.js
@@ -0,0 +1,5 @@
+function funkIt() {
+    canvasCtx.font = "32px sans-serif";
+    canvasCtx.fillStyle = "rgb(255,0,0)";
+    canvasCtx.fillText("I bet ur gaye!", 180, 220);
+}
