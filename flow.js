a737548023af06edb1e06a049024e3859e17080c
Gauss Jordan <gausjordan@outlook.com>
2023-07-09 00:47:18 +0200

    Cistka viska datoteka

--- flow.js
+++ flow.js
@@ -84,87 +84,84 @@ function generateContour(cPts) {
 
 controlPoints = generateControlPoints();
 
-let proba = catmullRomSolver(controlPoints, 0.5, 0.5, 0.5);
+let proba = catmullRom(controlPoints, 0.5, 0.5, 0.5);
 
 
-function catmullRomSolver(cPts, alpha, tension, position) {
+function catmullRom(p0, p1, p2, p3, alpha, tension, position) {
     
-    for (let point = 0; point < cPts.length-3; point++) {
+    let dist01 = Math.sqrt(
+                    Math.pow( (p1.x - p0.x), 2) +
+                    Math.pow( (p1.y - p0.y), 2) );
+    let dist12 = Math.sqrt(
+                    Math.pow( (p2.x - p1.x), 2) +
+                    Math.pow( (p2.y - p1.y), 2) );
+    let dist23 = Math.sqrt(
+                    Math.pow( (p3.x - p2.x), 2) +
+                    Math.pow( (p3.y - p2.y), 2) );
 
-        let dist01 = Math.sqrt(
-                     Math.pow( (cPts[point+1].x - cPts[point].x), 2) +
-                     Math.pow( (cPts[point+1].y - cPts[point].y), 2) );
-        let dist12 = Math.sqrt(
-                     Math.pow( (cPts[point+2].x - cPts[point+1].x), 2) +
-                     Math.pow( (cPts[point+2].y - cPts[point+1].y), 2) );
-        let dist23 = Math.sqrt(
-                     Math.pow( (cPts[point+3].x - cPts[point+2].x), 2) +
-                     Math.pow( (cPts[point+3].y - cPts[point+2].y), 2) );
+    let t01 = Math.pow(dist01, alpha);
+    let t12 = Math.pow(dist12, alpha);
+    let t23 = Math.pow(dist23, alpha);
 
-        let t01 = Math.pow(dist01, alpha);
-        let t12 = Math.pow(dist12, alpha);
-        let t23 = Math.pow(dist23, alpha);
-
-        let m1 = {
-            x:  (1 - tension) *
-                ( 2 - cPts[point+2].x - cPts[point+1].x + t12 *
-                    ( (cPts[point+1].x - cPts[point].x) / t01 -
-                    (cPts[point+2].x - cPts[point].x) / (t01 + t12)
-                    )
-                ),
-            y:  (1 - tension) *
-                ( 2 - cPts[point+2].y - cPts[point+1].y + t12 *
-                    ( (cPts[point+1].y - cPts[point].y) / t01 -
-                    (cPts[point+2].y - cPts[point].y) / (t01 + t12)
-                    )
+    let m1 = {
+        x:  (1 - tension) *
+            ( 2 - p2.x - p1.x + t12 *
+                ( (p1.x - p0.x) / t01 -
+                (p2.x - p0.x) / (t01 + t12)
                 )
-        };
-
-        let m2 = {
-            x:  (1 - tension) *
-                ( 2 - cPts[point+2].x - cPts[point+1].x + t12 *
-                    ( (cPts[point+3].x - cPts[point+2].x) / t23 -
-                    (cPts[point+3].x - cPts[point+1].x) / (t12 + t23)
-                    )
-                ),
-            y:  (1 - tension) *
-                ( 2 - cPts[point+2].y - cPts[point+1].y + t12 *
-                    ( (cPts[point+3].y - cPts[point+2].y) / t23 -
-                    (cPts[point+3].y - cPts[point+1].y) / (t12 + t23)
-                    )
+            ),
+        y:  (1 - tension) *
+            ( 2 - p2.y - p1.y + t12 *
+                ( (p1.y - p0.y) / t01 -
+                (p2.y - p0.y) / (t01 + t12)
                 )
-        };
+            )
+    };
 
-        let a = {
-            x: 2 * (cPts[point+1].x - cPts[point+2].x) + m1.x + m2.x,
-            y: 2 * (cPts[point+1].y - cPts[point+2].y) + m1.y + m2.y
-        };
+    let m2 = {
+        x:  (1 - tension) *
+            ( 2 - p2.x - p1.x + t12 *
+                ( (p3.x - p2.x) / t23 -
+                (p3.x - p1.x) / (t12 + t23)
+                )
+            ),
+        y:  (1 - tension) *
+            ( 2 - p2.y - p1.y + t12 *
+                ( (p3.y - p2.y) / t23 -
+                (p3.y - p1.y) / (t12 + t23)
+                )
+            )
+    };
 
-        let b = {
-            x: -3 * (cPts[point+1].x - cPts[point+2].x) - m1.x -m1.x -m2.x,
-            y: -3 * (cPts[point+1].y - cPts[point+2].y) - m1.y -m1.y -m2.y,
-        };
-        
-        let c = { x: m1.x, y: m1.y };
+    let a = {
+        x: 2 * (p1.x - p2.x) + m1.x + m2.x,
+        y: 2 * (p1.y - p2.y) + m1.y + m2.y
+    };
 
-        let d = { x: cPts[point+1].x, y: cPts[point+1].y };
+    let b = {
+        x: -3 * (p1.x - p2.x) - m1.x -m1.x -m2.x,
+        y: -3 * (p1.y - p2.y) - m1.y -m1.y -m2.y,
+    };
+    
+    let c = { x: m1.x, y: m1.y };
 
-        return {
-            x:  a.x * Math.pow(position, 3) +
-                b.x * Math.pow(position, 2) +
-                c.x * position +
-                d.x,
-            y:  a.y * Math.pow(position, 3) +
-                b.y * Math.pow(position, 2) +
-                c.y * position +
-                d.y,
-        };
-    }
+    let d = { x: p1.x, y: p1.y };
+
+    return {
+        x:  a.x * Math.pow(position, 3) +
+            b.x * Math.pow(position, 2) +
+            c.x * position +
+            d.x,
+        y:  a.y * Math.pow(position, 3) +
+            b.y * Math.pow(position, 2) +
+            c.y * position +
+            d.y,
+    };
 }
 
 canvasCtx.fillStyle = "rgba(255,0,0,255)";
 for (let i=0; i < 1; i+= 0.01) {
-    let temp = catmullRomSolver(controlPoints, 0.5, -2, i);
+    let temp = catmullRom(controlPoints[0], controlPoints[1], controlPoints[2], controlPoints[3], 0.5, -2, i);
     canvasCtx.fillRect(temp.x*3, temp.y*3, 3, 3);
 }
   
@@ -187,7 +184,7 @@ function drawPixelData(pixelData) {
 
 
 // Returns a 4-point-curve segment value at position "t"
-function catmullRom(p0, p1, p2, p3, t) {
+function catmullRomOld(p0, p1, p2, p3, t) {
     return (
         2 * p1 +
         t * (-p0 + p2) +
