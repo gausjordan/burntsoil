b396ff92092c9fa524673b5e32581b90d77da689
Gauss Jordan <gausjordan@outlook.com>
2023-07-23 15:26:16 +0200

    Ipak kubna interpolacija, prikladnija je, u uzasnom stanju

--- flow.js
+++ flow.js
@@ -50,20 +50,37 @@ function generateControlPoints() {
     let pointPositions = [];
     let pointHeights = [];
 
-    for (let i=0; i < 12; i++) {
-        pointHeights[i] = randomInteger(80, 600);
-    }
+    // for (let i=0; i < 10; i++) {
+    //     pointHeights[i] = randomInteger(80, 600);
+    // }
     
     // hardcoded debug points
-    pointPositions = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200];
+    // pointPositions = [100, 300, 400, 600, 700, 800, 900, 1000, 1100, 1200];
     // pointHeights = [100, 200, 100, 200, 100, 200, 100, 200];
 
-    let points = pointPositions.map((currentValue, index) => {
-        return {
-            y: pointPositions[index],
-            x: pointHeights[index]
-        }
-    });
+    // let points = pointPositions.map((currentValue, index) => {
+    //     return {
+    //         y: pointPositions[index],
+    //         x: pointHeights[index]
+    //     }
+    // });
+
+
+    let widthInPx = 0;
+    let points = [];
+    let newPosition = 0;
+    let newHeight = 0;
+
+    while (widthInPx <= flexMain.clientWidth) {
+        
+        newHeight = randomInteger(100, 820);
+        points.push ({
+            x: widthInPx + newPosition,
+            y: newHeight
+        });
+        newPosition = randomInteger(100, 250);
+        widthInPx += newPosition;
+    }
 
     return points;
 }
@@ -79,38 +96,65 @@ function generateContour(cPts) {
 }
 
 
+
+
+
 controlPoints = generateControlPoints();
+// controlPoints.length = 5;
 
-let proba = catmullRom(controlPoints, 0.5, 0.5, 0.5);
+// controlPoints[0].x = 0;
+// controlPoints[1].x = 200;
+// controlPoints[2].x = 400;
+// controlPoints[3].x = 600;
+// controlPoints[4].x = 800;
 
+// controlPoints[0].y = 200;
+// controlPoints[1].y = 600;
+// controlPoints[2].y = 400;
+// controlPoints[3].y = 100;
+// controlPoints[4].y = 200;
+
+console.log(controlPoints.length);
 
 
 // DEBUG draw
 
 canvasCtx.fillStyle = "rgba(0,0,255,255)";
 for (let i=0; i < controlPoints.length; i++) {
-    canvasCtx.fillRect(controlPoints[i].y, controlPoints[i].x, 6, 6);
+    canvasCtx.fillRect(controlPoints[i].x, controlPoints[i].y, 6, 6);
 }
 
 canvasCtx.fillStyle = "rgba(255,0,0,255)";
+  
+let px = 0;
+let y = 0;
 
+for (let seg = 0; seg < controlPoints.length-3; seg+=1) {
     
-    for (let j=0; j < controlPoints.length; j+=1) {
+    for (let t=0; t<1; t+=0.01) {
+        // This sorty worked, shifted
+        // y = cubic_interpolate(controlPoints[seg+0].y, controlPoints[seg+1].y, controlPoints[seg+2].y, controlPoints[seg+3].y, t);
+        // canvasCtx.fillRect( px + (controlPoints[seg+1].x-controlPoints[seg+0].x) * t , (canvasRef.height-y), 3, 3);
 
-        for (let i=0; i < 1; i+= 0.005) {
-            let temp1 = catmullRom(controlPoints[j+0], controlPoints[j+2], controlPoints[j+1], controlPoints[j+3], 0.5, 0.8, i);
-            canvasCtx.fillRect(temp1.y, temp1.x, 3, 3);
-        }
+        x = cubic_interpolate(controlPoints[seg+0].x, controlPoints[seg+1].x, controlPoints[seg+2].x, controlPoints[seg+3].x, t);
+        y = cubic_interpolate(controlPoints[seg+0].y, controlPoints[seg+1].y, controlPoints[seg+2].y, controlPoints[seg+3].y, t);
+        canvasCtx.fillRect( x, y, 3, 3);
     }
-
-
-
-testDraw();
-
-function testDraw() {
-
+    px += controlPoints[seg+1].y-controlPoints[seg+0].y;
+    
 }
 
+
+
+function cubic_interpolate(y0, y1, y2, y3, mu) {
+    let a0, a1, a2, a3, mu2;
+    mu2 = mu*mu;
+    a0 = y3 - y2 - y0 + y1; //p
+    a1 = y0 - y1 - a0;
+    a2 = y2 - y0;
+    a3 = y1;
+    return ( a0*mu*mu2 + a1*mu2 + a2*mu + a3 );
+}
 
 
 function drawPixelData(pixelData) {
